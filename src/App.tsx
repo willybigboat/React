59a177d339b29bb411ff117a77b199ba6ee7c12b
willybigboat <411630337@o365.tku.edu.tw>
2024-10-26 22:21:04 +0800

    html.css修改vite.css

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,43 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <body>
+      <div >
+        <p >
+        <h1>這是我的自我介紹</h1>
+        <h2>
+          <ul>
+            <li><strong>姓名:</strong><em>翁濬緯</em></li>
+            <li><strong>學號:</strong><em>411630337</em></li>
+            <li><strong>興趣:</strong><em>打羽球、看劇</em></li>
+          </ul>
+        </h2>
+        </p >
+        <img src="mom.jpg" width="500" />
+        <span>
+          <table>
+            <tr>
+              <th>~9/18</th>
+              <th>9/18~</th>
+            </tr>
+            <tr>
+              <td>迷你地鐵</td>
+              <td>迷你空管</td>
+            </tr>
+          </table>
+        </span>
+        <div >
+          <form action="/submit" method="post">
+            <label htmlFor="name">現在的心情:</label>
+            <input type="text" id="name" name="name" />
+            <input type="submit" value="送出" />
+          </form>
+          <a href="https://www.instagram.com/xi_06_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+            這是我最愛的媽媽 </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </body>
+  );
+};
 
-export default App
+export default App;
